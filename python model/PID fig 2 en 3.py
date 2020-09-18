91ba434641aa99f635eedee307a3f075cc43bd78
Hugo Bruins <hugobruins@gmail.com>
2020-09-18 16:20:23 +0200

    voor figuur 2 en 3

---
+++ python model/PID fig 2 en 3.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+T = 20              #starttemperatuur
+beta = 60           #gewenste temperatuur
+dt = 0.001          #tijdstap
+tijd = 25           #maximale tijd
+
+Kp = 1
+
+tijdarray = np.arange(0., tijd, dt)         #array voor tijd maken
+temp = np.array([])                         #array voor temperatuur 
+
+for t in tijdarray:
+    e = beta - T                            #berekent de fout, het verschil tussen de gewenste en huidige waarde
+    P = Kp * e 
+    
+    T = T + 50*dt * (1 - (T / 150))         #logistieke groei van temperatuur met een limiet van 150 graden   
+    if (P < 0):
+        T = T + P*dt   
+        
+    temp = np.append(temp,T)
+    
+#dit gedeelte maakt de plot met matplotlib 
+
+plt.figure(2)
+plt.grid(True)                                                 
+plt.plot(tijdarray,temp,label='temperatuur met Kp = 1')      
+plt.ylabel('temperatuur') 
+plt.xlabel('tijd')
+leg = plt.legend()
+
+
+    
+
+
+
