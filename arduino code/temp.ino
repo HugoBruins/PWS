f4423426eed8f2234eea29e0be1c347882394e9a
thomasritmeester <71404051+thomasritmeester@users.noreply.github.com>
2020-11-23 22:14:22 +0100

    Update temp.ino

--- arduino code/temp.ino
+++ arduino code/temp.ino
@@ -15,6 +15,4 @@ void loop()
   int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + 
                                             normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
   int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
-  
-  delay(10);
 }
