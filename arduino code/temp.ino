6aa6ac5c6fac3a192f72597b0efb92a4d9317299
thomasritmeester <71404051+thomasritmeester@users.noreply.github.com>
2020-11-23 19:44:05 +0100

    Create temp.ino

---
+++ arduino code/temp.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() 
+{
+  mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)
+}
+
+void loop()
+{
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+  
+  delay(10);
+}
